286905a21f6a002f42c8d135c1d9df32669bbad5
puppppet <puppet.training@iqinbox.com>
2016-07-20 11:49:01 -0600

    declare the users module and clean up formatting

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,21 +1,3 @@
-## site.pp ##
-
-# This file (/etc/puppetlabs/puppet/manifests/site.pp) is the main entry point
-# used when an agent connects to a master and asks for an updated configuration.
-#
-# Global objects like filebuckets and resource defaults should go in this file,
-# as should the default node definition. (The default node can be omitted
-# if you use the console and don't define any other nodes in site.pp. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.)
-
-## Active Configurations ##
-
-# PRIMARY FILEBUCKET
-# This configures puppet agent and puppet inspect to back up file contents when
-# they run. The Puppet Enterprise console needs this to display file contents
-# and differences.
-
 # Disable filebucket by default for all File resources:
 File { backup => false }
 
@@ -28,30 +10,12 @@ ini_setting { 'random ordering':
   value   => 'title-hash',
 }
 
-# DEFAULT NODE
-# Node definitions in this file are merged with node data from the console. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.
-
-# The default node definition matches any node lacking a more specific node
-# definition. If there are no other nodes in this file, classes declared here
-# will be included in every node's catalog, *in addition* to any classes
-# specified in the console for that node.
-
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
   notify { "Hello Silly, my name is most assuredly ${::hostname}": }
 
-  #file { '/etc/motd':
-  #  ensure => file,
-  #  owner => 'root',
-  #  group => 'root',
-  #  mode => '0644',
-  #  content => "Welcome, oh great Puppppet Master!\n",
-  #}
-  
   exec { 'welcome':
   command => "/usr/local/bin/cowsay 'Welcome to ${::fqdn}!' > /etc/motd", 
   creates => '/etc/motd',
@@ -62,4 +26,8 @@ node default {
   ip => '127.0.0.1',
   host_aliases => ['foo', 'foobar'],
   }
+
+  #Exercise 9.2
+  include users
+
 }
