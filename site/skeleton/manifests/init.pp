2baca2aef04492c61740fe3adadb359de736b4ff
puppppet <puppppet>
2016-07-20 18:52:35 +0000

    typo

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -10,7 +10,7 @@ class skeleton {
     ensure => file,
     owner  => 'root',
     group  => 'root',
-    mode   => '0644'
+    mode   => '0644',
     source => 'puppet:///modules/skeleton/bashrc'
   }
 
