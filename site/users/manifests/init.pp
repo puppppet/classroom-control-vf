535f443fc47108d1467ca5af66fb1e537857f540
puppppet <puppppet>
2016-07-21 21:56:34 +0000

    moved admins.pp into init.pp

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -2,6 +2,18 @@ class users {
 
   user { 'fundamentals':
     ensure => present,
+
+  users::managed_user{ 'jose': }
+  users::managed_user{ 'alice': }
+  users::managed_user{ 'chen': }
+  users::managed_user{ 'superman':
+    group => ['staff', 'security']
   }
 
+  group { 'staff':
+    ensure => present,
+  }
+  group { 'security':
+    ensure => present,
+  }
 }
