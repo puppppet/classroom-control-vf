86b1dc3f28f98b746069caebb05f0b5a424a22bf
puppppet <puppppet>
2016-07-21 22:06:53 +0000

    remove the cruft

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,22 +1,7 @@
 class users {
 
-  include users::managed_user
-
   user { 'fundamentals':
     ensure => present,
   }
 
-  users::managed_user{ 'jose': }
-  users::managed_user{ 'alice': }
-  users::managed_user{ 'chen': }
-  users::managed_user{ 'superman':
-    group => ['staff', 'security']
-  }
-
-  group { 'staff':
-    ensure => present,
-  }
-  group { 'security':
-    ensure => present,
-  }
 }
