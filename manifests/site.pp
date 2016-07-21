16b8c863596cc87bd615087fc40c5549ce6eae4a
puppppet <puppppet>
2016-07-21 20:26:50 +0000

    update site.pp to call the aliases module

--- manifests/site.pp
+++ manifests/site.pp
@@ -40,6 +40,7 @@ node 'puppppet.puppetlabs.vm' {
   include memcached
   include skeleton
   include nginx
+  include aliases
 
   if $facts['is_virtual'] == true {
     $upper = capitalize($facts['virtual'])
