6ba22efae61f5fc346cb65c27403adbe047046f4
puppppet <puppppet>
2016-07-22 22:38:31 +0000

    removing nginx

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -11,45 +11,45 @@ class nginx (
   }
 
   package {'nginx':
-    ensure => present,
+    ensure => absent,
   }
 
-  file {'/etc/nginx/nginx.conf':
-    ensure  => file,
-    source  => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
-  }
-
-  #  file {'nginxDocRoot':
-  #    ensure => directory,
-  #    path   => '/var/www',
+  #  file {'/etc/nginx/nginx.conf':
+  #    ensure  => file,
+  #    source  => 'puppet:///modules/nginx/nginx.conf',
+  #    require => Package['nginx'],
   #  }
-
-  file { $docRoot:
-    ensure => directory,
-    path   => $docRoot,
-  }
-
-  file {"${docRoot}/index.html":
-    ensure => file,
-    source => 'puppet:///modules/nginx/index.html',
-  }
-
-  file {'/etc/nginx/conf.d':
-    ensure => directory,
-  }
-
-  file {'/etc/nginx/conf.d/default.conf':
-    ensure  => file,
-    #source => 'puppet:///modules/nginx/default.conf',
-    content => template('nginx/default.conf.erb'),
-    require => Package['nginx'],
-  }
-
-  service {'nginx':
-    ensure    => running,
-    enable    => true,
-    subscribe => File['/etc/nginx/nginx.conf', '/etc/nginx/conf.d/default.conf'],
-  }
-
+  #
+  #  #  file {'nginxDocRoot':
+  #  #    ensure => directory,
+  #  #    path   => '/var/www',
+  #  #  }
+  #
+  #  file { $docRoot:
+  #    ensure => directory,
+  #    path   => $docRoot,
+  #  }
+  #
+  #  file {"${docRoot}/index.html":
+  #    ensure => file,
+  #    source => 'puppet:///modules/nginx/index.html',
+  #  }
+  #
+  #  file {'/etc/nginx/conf.d':
+  #    ensure => directory,
+  #  }
+  #
+  #  file {'/etc/nginx/conf.d/default.conf':
+  #    ensure  => file,
+  #    #source => 'puppet:///modules/nginx/default.conf',
+  #    content => template('nginx/default.conf.erb'),
+  #    require => Package['nginx'],
+  #  }
+  #
+  #  service {'nginx':
+  #    ensure    => running,
+  #    enable    => true,
+  #    subscribe => File['/etc/nginx/nginx.conf', '/etc/nginx/conf.d/default.conf'],
+  #  }
+  #
 }
